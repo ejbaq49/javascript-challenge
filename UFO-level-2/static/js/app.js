6b7db6356b4afe77d276b2592d35e04843a64242
EJBaquet <ejbaquet@yahoo.com>
2020-08-24 17:44:22 -0400

    copied files to begin Level 2

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,35 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+// Create event handlers
+button.on("click", filterDate);
+form.on("submit", filterDate);
+
+// Add function to filter by date
+function filterDate() {
+  // Prevent refresh
+  d3.event.preventDefault();
+  // Get date from input
+  var inputElement = d3.select(".form-control");
+  var inputValue = inputElement.property("value");
+  // Filter data
+  filteredData = tableData.filter(
+    (sighting) => sighting.datetime === inputValue
+  );
+  // Clear table
+  tbody.html("");
+  // Loop through data and add row to table body
+  filteredData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    // Loop through each document and add td
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
