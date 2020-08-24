645e7ee7d08dd6c648f81d23242b95e97c220677
EJBaquet <ejbaquet@yahoo.com>
2020-08-24 16:04:17 -0400

    filterDate test

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,28 +7,27 @@ var button = d3.select("#filter-btn");
 var form = d3.select("form");
 
 // Create event handlers
-// button.on("click")
+button.on("click", filterDate);
+form.on("submit", filterDate);
 
 // Add function to filter by date
-function filterDate(prmData, prmDate) {
+function filterDate() {
   // Prevent refresh
   d3.event.preventDefault();
+  // Get date from input
+  var inputElement = d3.select(".form-control");
+  var inputValue = inputElement.property("value");
   // Filter data
-  filteredData = prmData.filter((sighting) => sighting.datetime >= prmDate);
-  return filteredData;
-}
-
-// Capture date parameter on submit/click
-
-// Create new filtered data set using function and date parameter
-
-// Replace tableData in loop below with filteredData from function
-// Loop through data and add row to table body
-tableData.forEach((sighting) => {
-  var row = tbody.append("tr");
-  // Loop through each document and add td
-  Object.entries(sighting).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
+  filteredData = tableData.filter(
+    (sighting) => sighting.datetime >= inputValue
+  );
+  // Loop through data and add row to table body
+  filteredData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    // Loop through each document and add td
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
-});
+}
